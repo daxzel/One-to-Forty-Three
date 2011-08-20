c226f23935a14e447921547c3bc281e71c459322
Andrey_Tsarevskiy <daxzel@bk.ru>
2011-08-20 11:36:06 +0400

    Errors!

--- config/application.rb
+++ config/application.rb
@@ -26,8 +26,6 @@ module OneTo43
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
-    config.count_cars_view = 4
-
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ru
---
+++ spec/models/configs_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe Configs do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
